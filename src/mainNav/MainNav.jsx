378d46c7b4a4c4ef20b06da54114c9c9ed45beb0
GitAarontell <adbertell2@gmail.com>
2023-04-06 17:32:02 -0700

    fixed symbols

--- src/mainNav/MainNav.jsx
+++ src/mainNav/MainNav.jsx
@@ -55,8 +55,7 @@ class MainNav extends React.Component {
                 <div className='navBar grid'>
                     <div className='home grid'>
 
-                        <img className='storeImg' src="https://cdns.iconmonstr.com/wp-content/assets/preview/2017/96/iconmonstr-shop-13.png" alt="store" />
-
+                        <svg className='storeImg' xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24"><path d="M10 9v-1.098l1.047-4.902h1.905l1.048 4.9v1.098c0 1.067-.933 2.002-2 2.002s-2-.933-2-2zm5 0c0 1.067.934 2 2.001 2s1.999-.833 1.999-1.9v-1.098l-2.996-5.002h-1.943l.939 4.902v1.098zm-10 .068c0 1.067.933 1.932 2 1.932s2-.865 2-1.932v-1.097l.939-4.971h-1.943l-2.996 4.971v1.097zm-4 2.932h22v12h-22v-12zm2 8h18v-6h-18v6zm1-10.932v-1.097l2.887-4.971h-2.014l-4.873 4.971v1.098c0 1.066.933 1.931 2 1.931s2-.865 2-1.932zm15.127-6.068h-2.014l2.887 4.902v1.098c0 1.067.933 2 2 2s2-.865 2-1.932v-1.097l-4.873-4.971zm-.127-3h-14v2h14v-2z"/></svg>
                         <div className='storeTitle'>
                             <Link to='/'>Manners</Link>
                         </div>
@@ -73,7 +72,7 @@ class MainNav extends React.Component {
 
                     <div className='cart grid'>
                         <Link className='cartImg' to='/cart'>
-                            <img src="https://cdns.iconmonstr.com/wp-content/assets/preview/2013/96/iconmonstr-shopping-cart-3.png" alt="shopping cart" />
+                            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24"><path d="M10 20.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm6.304-17l-3.431 14h-2.102l2.541-11h-16.812l4.615 13h13.239l3.474-14h2.178l.494-2h-4.196z"/></svg>
                             {this.setCount(this.props.info.logTotal)}
                         </Link>
                     </div>
